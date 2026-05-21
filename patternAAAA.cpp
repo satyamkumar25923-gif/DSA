1526c7b1750a508dd4b7e4c0c5ad68e8d4614a41
satyamkumar25923 <satyamkumar25923@gmail.com>
2026-05-22 02:33:50 +0530

    initial commit

---
+++ patternAAAA.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std; 
+int main(){
+int n ; 
+int i = 1;
+int j =1 ;
+cout  << "enter a number : " ;
+cin >> n ;
+while(i<=n){
+    
+    int j = 1;
+    while(j<=n){
+        char ch = 'A'+ i-1;
+        cout << ch ;
+        j++;
+
+    }
+    cout << "\n";
+    i++;
+}
+
+    return 0;
+}
