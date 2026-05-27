d08d4a4a8213183e685286522b94b6534c14abf9
satyamkumar25923 <satyamkumar25923@gmail.com>
2026-05-28 02:36:30 +0530

    initial commit

---
+++ LeetCode/Diff.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+int n ; 
+cout << " enter a number : ";
+cin >> n ;
+int digit; 
+int pro = 1;
+int sum = 0;
+int diff;
+while(n!=0){
+    digit = n % 10 ;
+    pro = pro * digit ;
+    sum = sum + digit ; 
+    n = n / 10 ;
+}
+diff = pro - sum ; 
+cout << diff ;
+} 
