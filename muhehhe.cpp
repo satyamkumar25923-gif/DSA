765eb7e7f7d8edf19511e7ff2c1220eea2fbfca4
Satyam Kumar <satyamkumar25923@gmail.com>
2026-05-30 23:38:13 +0530

    Change loop variable from 'm' to 'n'
    Refactor input variable from 'm' to 'n' and update loops accordingly.

--- muhehhe.cpp
+++ muhehhe.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std; 
 int main(){
-int m ; 
+int n ; 
 int i = 1;
 cout  << "enter a number : " ;
-cin >> m ;
-while(i<=m){
+cin >> n ;
+while(i<=n){
     int j = 1;
-    while(j<=m){
+    while(j<=n){
         cout << j;
         j++;
     }
