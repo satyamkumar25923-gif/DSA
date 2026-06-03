94a5567901c6845ebd0f5a2a706e24d58e42f107
satyamkumar25923 <satyamkumar25923@gmail.com>
2026-06-04 03:09:57 +0530

    initial commit

---
+++ LeetCode/Q233.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+        int n ; 
+        std::cout << "enter a num : ";
+        std::cin >> n ; 
+        int org = n ;
+        int digit = 0 ;
+        int  count = 0 ; 
+        while(n!=0){
+            while(n!=0){
+                digit = n % 10 ;
+                if(digit == 1){
+                    count++ ;
+                }
+                n = n / 10 ;
+            }
+            n = org - 1 ;
+            org = n ;
+        }
+        std::cout<< count  << "\n";
+}
